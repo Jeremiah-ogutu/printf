023e223cd71fb57f1349a3841ad0ca4855256e40
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-10-14 03:08:42 -0700

    creation of print_char file holding the 'c' specifire code

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <unistd.h>
+/**
+ * _putchar-it prints the character
+ * @c:character to be printed
+ * Return:1 success
+ */
 
 
 int _putchar(char c)
--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #ifndef 
 #define
 int _putchar(char c);
+int printf_char(va_list val);
 
 #endif
