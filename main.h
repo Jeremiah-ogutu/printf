05feb63cbf3a235b6abd3c4898d0d443e4be8d8a
LisetteMurugi <lisettendwiga4@gmail.com>
2023-10-14 15:00:15 -0700

    printf

--- main.h
+++ main.h
@@ -1,6 +1,9 @@
-#ifndef 
-#define
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 int printf_char(va_list val);
+int printf_string(va_list val);
+int _strlenc(const char *str);
+int _strlen(char *str);
 
 #endif
