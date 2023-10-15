2c773b0521507f4ce648b275f259aeb4bfcfd7b1
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-10-14 18:15:43 -0700

    creation of d and i specifier

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H 
 #define MAIN_H
 #include <stdarg.h>
+#include <stddef.h>
+#include <string.h>
 typedef struct format
 {
 	char *id;
@@ -12,5 +14,8 @@ int printf_char(va_list val);
 int print_special_char(void);
 int _strlenc(const char *str);
 int _strlen(char *str);
+int printf_integer(va_list args);
+int printf_int(va_list args);
+int print_string(va_list val);
 
 #endif
