5ba00d97c17bb40c81a0f305b88d8e5839c0dd84
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-10-18 13:53:36 +0300

    correction on binary

--- print_binary.c
+++ print_binary.c
@@ -3,12 +3,12 @@
 #include <limits.h>
 /**
  *printf_binary-fuction that print the binary number
- *@args:argument holder for intager
+ *@val:argument holder for intager
  *Return:printed char
  */
-int printf_binary(va_list args)
+int printf_binary(va_list val)
 {
-	unsigned int num = va_arg(args, unsigned int);
+	unsigned int num = va_arg(val, unsigned int);
 	int printed_chars = 0;
 	int bitMask = 1;
 	size_t i;
